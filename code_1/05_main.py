83682ef25bf160cb6db9188e8d5037a1c5164492
rohanmaiti <maitirohan2019@gmail.com>
2025-08-06 19:21:00 +0530

    learn: python header, cookie in request

--- code_1/05_main.py
+++ code_1/05_main.py
@@ -1,6 +1,8 @@
 #** COOKIE PARAMETER
 from typing import Annotated;
-from fastapi import FastAPI, Cookie
+from fastapi import FastAPI, Cookie, Header
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -9,3 +11,45 @@ def handle_cookie(item: Annotated[str, Cookie()] = None):
     return {
         "cookie": item
     }
+
+
+# # ** Header() 
+# @app.post('/get-header-content')
+# def getHeader(x_auth: Annotated[ str | None, Header() ]=None):
+#     return {
+#         "Printing header": x_auth
+#     }
+
+# It is possible to receive duplicate headers. That means, the same header with multiple values.
+# You will receive all the values from the duplicate header as a Python list
+@app.post('/get-header-content')
+def getHeader(x_auth: Annotated[ list[str] | None, Header() ]=None):
+    return {
+        "Printing header": x_auth
+    }
+
+
+# cookie model 
+
+# class Cookies(BaseModel):
+#     session_id: str
+#     fatebook_tracker: str | None = None
+#     googall_tracker: str | None = None
+
+
+# @app.get("/items/")
+# async def read_items(cookies: Annotated[Cookies, Cookie()]):
+#     return cookies
+
+# header model
+class CommonHeaders(BaseModel):
+    host: str
+    save_data: bool
+    if_modified_since: str | None = None
+    traceparent: str | None = None
+    x_tag: list[str] = []
+
+
+@app.get("/items/")
+async def read_items(headers: Annotated[CommonHeaders, Header()]):
+    return headers
---
+++ code_1/06_main.py
@@ -0,0 +1,91 @@
+#* Return type of any function 
+
+from fastapi import FastAPI, Body;
+from pydantic import BaseModel;
+from typing import Annotated, Any;
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: list[str] = []
+
+
+@app.post("/items/")
+async def create_item(item: Item) -> Item:
+    return item
+
+@app.get("/items/")
+async def read_items() -> list[Item]:
+    return [
+        Item(name="Portal Gun", price=42.0),
+        Item(name="Plumbus", price=32.0),
+    ]
+
+# response_model Parameter
+
+@app.get('/items/', response_model=Item)
+async def create_item(item: Item) -> Any:
+    return Item
+
+@app.post('/items/', respose_model=list[Item])
+async def read_item(listOfItem: Annotated[ list[Item], Body()]= None) -> Any:
+    return {
+        "all_data": listOfItem
+    }
+
+
+# email validator 
+# To use EmailStr, first install email-validator.
+#  pip install email-validator
+# or 
+# $ pip install "pydantic[email]"
+
+from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
+
+app = FastAPI()
+
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+# Don't do this in production!
+@app.post("/user/")
+async def create_user(user: UserIn) -> UserIn:
+    return user
+
+
+ # we should never return plain passowrd like this 
+ # so we can make 2 models one for request and anther for response 
+
+class Request(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    
+class Response(BaseModel):
+    username:str
+    email: EmailStr
+    description: Any = None    
+
+@app.post(
+    '/post-route', response_model=Response
+)
+async def handlePost(data: [Annotated[Request, Body()]] = None) -> any:
+    return {
+        "username": data.username,
+        "email": data.email,
+        "description": {
+            "name" : "rohan maiti",
+            "age": "21"
+        }
+    }
